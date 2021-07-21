1d99774695a4c0ad8dbbd94fee3de14771496909
nina <niny997@gmail.com>
2021-07-21 20:18:19 +0200

    Project added.

---
+++ Four_in_a_row/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Four_in_a_row
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.color_1 = new System.Windows.Forms.Button();
+            this.color_2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(553, 383);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(582, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "NEW GAME";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(582, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // color_1
+            // 
+            this.color_1.Location = new System.Drawing.Point(26, 19);
+            this.color_1.Name = "color_1";
+            this.color_1.Size = new System.Drawing.Size(29, 27);
+            this.color_1.TabIndex = 5;
+            this.color_1.Text = "1";
+            this.color_1.UseVisualStyleBackColor = true;
+            this.color_1.Click += new System.EventHandler(this.color_1_Click);
+            // 
+            // color_2
+            // 
+            this.color_2.Location = new System.Drawing.Point(61, 19);
+            this.color_2.Name = "color_2";
+            this.color_2.Size = new System.Drawing.Size(29, 27);
+            this.color_2.TabIndex = 6;
+            this.color_2.Text = "2";
+            this.color_2.UseVisualStyleBackColor = true;
+            this.color_2.Click += new System.EventHandler(this.color_2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.panel1.Location = new System.Drawing.Point(26, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(29, 27);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel2.Location = new System.Drawing.Point(61, 74);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(29, 27);
+            this.panel2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(579, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.color_1);
+            this.groupBox1.Controls.Add(this.color_2);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Location = new System.Drawing.Point(582, 94);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(116, 111);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Color change";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(582, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 408);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button color_1;
+        private System.Windows.Forms.Button color_2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Four_in_a_row/Form1.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Four_in_a_row
+{
+
+    public partial class Form1 : Form
+    {
+
+        static Graphics canvas;
+        static int width;
+        static int height;
+
+        static List<List<int>> grid_list = new List<List<int>>();
+
+        static int x_cells = 10;
+        static int y_cells = 7;
+        static int winner = 0;
+
+        static int x1;
+        static int x2;
+        static int y1;
+        static int y2;
+
+        static float cell_width;
+        static float cell_height;
+
+        static float elipse_width;
+        static float elipse_height;
+
+        static int grid_x;
+        static int grid_y;
+
+        static Pen lines = new Pen(Color.LightGray, 1);
+        static Pen rectangle = new Pen(Color.LightGray, 10);
+        static Pen pen_1 = new Pen(Color.Black, 1);
+        static Pen pen_2 = new Pen(Color.Black, 2);
+
+        static Color coin_color1 = Color.FromArgb(255, 128, 128);
+        static Color coin_color2 = Color.FromArgb(255, 255, 192);
+
+        static SolidBrush clear_color = new SolidBrush(Color.White);
+        static SolidBrush brush_1 = new SolidBrush(coin_color1);
+        static SolidBrush brush_2 = new SolidBrush(coin_color2);
+
+        static int active_player = 1;
+        static bool is_grid_set = true;
+        static bool color_change = false;
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            canvas = pictureBox1.CreateGraphics();
+
+            RectangleF bounds = canvas.VisibleClipBounds;
+            width = (int)Math.Floor(bounds.Width);
+            height = (int)Math.Floor(bounds.Height);
+
+            cell_width = width / x_cells;
+            cell_height = height / y_cells;
+
+            elipse_width = 9 * (cell_width / 11);
+            elipse_height = 9 * (cell_height / 11);
+
+            
+            
+        }
+
+        static void initialize_grid()
+        {
+            grid_list.Clear();
+
+            for (int i = 0; i < y_cells; i++)
+            {
+                List<int> row = new List<int>();
+
+                for (int j = 0; j < x_cells; j++)
+                {
+                    row.Add(0);
+                }
+
+                grid_list.Add(row);
+            }
+        }
+
+        static void initialize_game()
+        {
+            canvas.FillRectangle(clear_color, 0, 0, width, height);
+            draw_lines();
+
+            initialize_grid();
+
+            winner = 0;
+            is_grid_set = true;
+
+        }
+
+        // DRAW 
+
+            // all lines
+
+        static void draw_lines()
+        {
+            int x = 1;
+            int y = 1;
+
+            canvas.DrawRectangle(rectangle, 0, 0, width, height);
+
+            for (int i = 0; i < y_cells - 1; i++)
+            {
+                canvas.DrawLine(lines, 0, y * (height / y_cells), width, y * (height / y_cells));
+                y++;
+            }
+
+            for (int i = 0; i < x_cells - 1; i++)
+            {
+                canvas.DrawLine(lines, x * (width / x_cells), 5, x * (width / x_cells), height - 5);
+                x++;
+            }
+        }
+
+            // a coin
+
+        static void draw_coin(int grid_x, int grid_y, Brush brush, Pen pen)
+        {
+            
+            float x = grid_x * cell_width + cell_width / 11;
+            float y = grid_y * cell_height + cell_height / 11;
+
+            canvas.FillEllipse(brush, x, y, elipse_width, elipse_height);
+            canvas.DrawEllipse(pen, x, y, elipse_width, elipse_height);
+
+            if (color_change == false)
+            {
+                if (active_player == 1)
+                {
+                    grid_list[grid_y][grid_x] = 1;
+                }
+                else
+                {
+                    grid_list[grid_y][grid_x] = 2;
+                }
+            }
+        }
+
+        // Is grid[y][x] in bounds?
+
+        public static bool is_in_bounds(int x, int y)
+        {
+            if (x < x_cells && y < y_cells && x >= 0 && y >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // How many coins in one direction?
+
+        static int count_coins(int x, int y, int d_x, int d_y, int symbol, 
+            out int point_x, out int point_y)
+        {
+            int coin_ammount = 0;
+            point_x = x;
+            point_y = y;
+
+            int a = x + d_x;
+            int b = y + d_y;
+
+            while (true)
+            {
+                if (is_in_bounds(a, b) == true)
+                {
+                    if (grid_list[b][a] == symbol)
+                    {
+                        coin_ammount++;
+                        point_x = a;
+                        point_y = b;
+                        a += d_x;
+                        b += d_y;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return coin_ammount;
+        }
+
+        // Is game over? (Do we have a winner?)
+
+        static bool is_game_over(int x, int y)
+        {
+            if (count_coins(x, y, 0, 1, active_player, out x1, out y1) + count_coins(x, y, 0, -1, active_player, out x2, out y2) + 1 >= 4)
+            {
+                x1 = x1 * Convert.ToInt32(cell_width) + Convert.ToInt32(cell_width) / 2;
+                y1 = y1 * Convert.ToInt32(cell_height) + Convert.ToInt32(cell_height) / 2;
+                x2 = x2 * Convert.ToInt32(cell_width) + Convert.ToInt32(cell_width) / 2;
+                y2 = y2 * Convert.ToInt32(cell_height) + Convert.ToInt32(cell_height) / 2;
+
+                return true;
+
+            }
+            else if (count_coins(x, y, 1, 1, active_player, out x1, out y1) + count_coins(x, y, -1, -1, active_player, out x2, out y2) + 1 >= 4)
+            {
+                x1 = x1 * Convert.ToInt32(cell_width) + Convert.ToInt32(cell_width) / 2;
+                y1 = y1 * Convert.ToInt32(cell_height) + Convert.ToInt32(cell_height) / 2;
+                x2 = x2 * Convert.ToInt32(cell_width) + Convert.ToInt32(cell_width) / 2;
+                y2 = y2 * Convert.ToInt32(cell_height) + Convert.ToInt32(cell_height) / 2;
+
+                return true;
+            }
+            else if (count_coins(x, y, 1, 0, active_player, out x1, out y1) + count_coins(x, y, -1, 0, active_player, out x2, out y2) + 1 >= 4)
+            {
+                x1 = x1 * Convert.ToInt32(cell_width) + Convert.ToInt32(cell_width) / 2;
+                y1 = y1 * Convert.ToInt32(cell_height) + Convert.ToInt32(cell_height) / 2;
+                x2 = x2 * Convert.ToInt32(cell_width) + Convert.ToInt32(cell_width) / 2;
+                y2 = y2 * Convert.ToInt32(cell_height) + Convert.ToInt32(cell_height) / 2;
+
+                return true;
+            }
+            else if (count_coins(x, y, 1, -1, active_player, out x1, out y1) + count_coins(x, y, -1, 1, active_player, out x2, out y2) + 1 >= 4)
+            {
+                x1 = x1 * Convert.ToInt32(cell_width) + Convert.ToInt32(cell_width) / 2;
+                y1 = y1 * Convert.ToInt32(cell_height) + Convert.ToInt32(cell_height) / 2;
+                x2 = x2 * Convert.ToInt32(cell_width) + Convert.ToInt32(cell_width) / 2;
+                y2 = y2 * Convert.ToInt32(cell_height) + Convert.ToInt32(cell_height) / 2;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Get grid_y
+
+        public static void get_grid_y(int x, out int grid_y)
+        {
+            grid_y = y_cells - 1;
+
+            for (int i = 0; i < y_cells; i++)
+            {
+                if (grid_list[y_cells - 1 - i][x] == 0)
+                {
+                    break;
+                }
+
+                grid_y -= 1;
+            }
+        }
+
+        // Get grid_x
+
+        static void pix_to_grid(int pix_x, out int grid_x)
+        {
+            int cell_width = width / (x_cells);
+            
+            double gridx_ = pix_x / cell_width;
+            grid_x = (int)Math.Ceiling(gridx_);
+
+        }
+
+        // NEW GAME BUTTON
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            initialize_game();
+
+            label1.Text = "Turn: player " + active_player;
+            label2.Text = "";
+            
+        }
+
+        // EXIT BUTTON
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+     
+        // Change your color
+
+            // Player 1:
+
+        private void color_1_Click(object sender, EventArgs e)
+        {
+            DialogResult color_1_result = colorDialog1.ShowDialog();
+
+            if (color_1_result == DialogResult.OK)
+            {
+                panel1.BackColor = colorDialog1.Color;
+                brush_1.Color = colorDialog1.Color;
+
+                color_change = true;
+
+                for (int i = 0; i < y_cells; i++)
+                {
+                    for (int j = 0; j < x_cells; j++)
+                    {
+                        if (grid_list[i][j] == 1)
+                        {
+                            draw_coin(j, i, brush_1, pen_1);
+                        }
+                    }
+                }
+
+                color_change = false;
+
+                if (winner != 0)
+                {
+                    canvas.DrawLine(pen_2, x1, y1, x2, y2);
+                }
+
+            }
+        }
+
+            // Player 2:
+
+        private void color_2_Click(object sender, EventArgs e)
+        {
+            DialogResult color_2_result = colorDialog2.ShowDialog();
+
+            if (color_2_result == DialogResult.OK)
+            {
+                panel2.BackColor = colorDialog2.Color;
+                brush_2.Color = colorDialog2.Color;
+
+                color_change = true;
+
+                for (int i = 0; i < y_cells; i++)
+                {
+                    for (int j = 0; j < x_cells; j++)
+                    {
+                        if (grid_list[i][j] == 2)
+                        {
+                            draw_coin(j, i, brush_2, pen_1);
+                        }
+                    }
+                }
+
+                color_change = false;
+
+                if (winner != 0)
+                {
+                    canvas.DrawLine(pen_2, x1, y1, x2, y2);
+                }
+            }
+        }
+
+
+        // CANVAS MOUSE CLICK
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int mouse_x = e.X;
+
+            if (is_grid_set == true)
+            {
+                pix_to_grid(mouse_x, out grid_x);
+                get_grid_y(grid_x, out grid_y);
+
+                if (winner == 0 && grid_y > -1)
+                {
+                    if (active_player == 1)
+                    {
+                        draw_coin(grid_x, grid_y, brush_1, pen_1);
+                    }
+                    else
+                    {
+                        draw_coin(grid_x, grid_y, brush_2, pen_1);
+                    }
+
+
+                    if (is_game_over(grid_x, grid_y) == true)
+                    {
+                        label2.Text = "Player " + active_player.ToString() + " Won!";
+                        winner = active_player;
+                        canvas.DrawLine(pen_2, x1, y1, x2, y2);
+                        is_grid_set = false;
+                    }
+
+                    if (is_grid_set == true)
+                    {
+                        if (active_player == 1)
+                        {
+                            active_player = 2;
+                            label1.Text = "Turn: player " + active_player;
+                        }
+                        else
+                        {
+                            active_player = 1;
+                            label1.Text = "Turn: player " + active_player;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Refresh();
+
+            initialize_game();
+
+            label1.Text = "Turn: player " + active_player;
+            label2.Text = "";
+
+        }
+    }
+}
